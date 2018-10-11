0345448c1684e0db1413e25ec35fad16da81713a
nmynhier <43651565+nmynhier@users.noreply.github.com>
2018-10-11 07:47:06 -0700

    Update operators.py

--- operators.py
+++ operators.py
@@ -1,35 +1,36 @@
-x = 9
+x = 9           
 y = 3
 
-print(x+y)
-print(x-y)
-print(x*y)
-print(x/y)
-print(x%y)
-print(x**y)
+# Arithmetic operators
+print(x+y)    #addition
+print(x-y)    #subtraction
+print(x*y)    #multiplication
+print(x/y)    #division
+print(x%y)    #modulus
+print(x**y)   #exponentiation
 x = 9.191823
-print(x//y)
+print(x//y)   #florr division
 
-x = 9
-x += 3
+#assignment operators
+x = 9         #set x = 9
+x += 3        #x = x + 3
 print(x)
 x = 9
-x -= 3
+x -= 3        #x = x - 3
 print(x)
-x *= 3
+x *= 3        #x = x * 3
 print(x)
-x /= 3
+x /= 3        #x = x / 3
 print(x)
-x **= 3
+x **= 3       #x = x ** 3
 print(x)
 
+#comparison operators
 x = 9
 y = 3
-print(x==y)
-print(x!=y)
-print(x>y)
-print(x<y)
-print(x>=y)
-print(x<=y)
-
-print("IT WORKED")
+print(x==y)   #true if x equals y, false otherwise
+print(x!=y)   #True if x does not equal y, false otherwise
+print(x>y)    #True if x is greater than y, false otherwise
+print(x<y)    #True if x is less than y, false otherwise
+print(x>=y)   #True if x is greater than or equal to y, false otherwise
+print(x<=y)   #True if x is less than or equal to y, false otherwise
