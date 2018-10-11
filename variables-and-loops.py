41fcb48cf202ea625b0b06fbef2c6e0be0d8ab48
nmynhier <43651565+nmynhier@users.noreply.github.com>
2018-10-11 07:39:18 -0700

    Update variables-and-loops.py

--- variables-and-loops.py
+++ variables-and-loops.py
@@ -1,18 +1,24 @@
-import numpy as np
+import numpy as np				#we use numpy for lost of things
 
 def main():
-	i = 0
-	n = 10
-	x = 119.0
+	i = 0					#integers can be declared with a number
+	n = 10					#here is another integer
+	x = 119.0				#floaring point nums are declared with a "."
 	
-	y = np.zeros(n,dtype=float)
+# we can use numpy to declare arrays quickly
 	
-	for i in range(n):
-		y[i] = 2.0 * float(i) + 1
+	y = np.zeros(n,dtype=float)		#declaring 10 zeros as floats using np
+	
+# we can use loops to iterate with a variable
+	
+	for i in range(n):			#i in range [0,n-1]
+		y[i] = 2.0 * float(i) + 1	#set y = 2i+1 as floats
 		
+	#we can also simply iterate through a variable
 	for y_element in y:
 		print(y_element)
 		
+#execute the main function
 if __name__ == "__main__":
 	main()
 	
