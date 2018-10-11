a25b5069166dc2e2c1076a946588cf24b6800be7
nmynhier <43651565+nmynhier@users.noreply.github.com>
2018-10-11 07:54:02 -0700

    Update data_types_continued.py

--- data_types_continued.py
+++ data_types_continued.py
@@ -6,26 +6,26 @@ print(type(s)) 			# say str
 #boolean
 
 yes = True
-print(type(yes))
+print(type(yes))		#boolean truw
 
 no = False
-print(type(no))
+print(type(no))			#boolean false
 
 #list -- ordered and changeable
 
 alpha_list = ["a", "b", "c"]	#list initialization
-print(type(alpha_list))
-print(type(alpha_list[0]))
-alpha_list.append("d")
-print(alpha_list)
+print(type(alpha_list))		#will say tuple
+print(type(alpha_list[0]))	#will say string
+alpha_list.append("d")		#will add "d" to the list end
+print(alpha_list)		#will print list
 
 #tuple -- ordered and unchangebale
 
-alpha_tuple = ("a", "b", "c")
-print(type(alpha_tuple))
+alpha_tuple = ("a", "b", "c")	#tuple initialization
+print(type(alpha_tuple))	#will say tuple
 
 try:
-	alpha_tuple[2] = "d"
-except TypeError:
-	print("we cant add the elements to tuples!")
-print(alpha_tuple)
+	alpha_tuple[2] = "d"	#attemp the following line
+except TypeError:		#wont work and will raise TypeError
+	print("we cant add the elements to tuples!")	#print this message
+print(alpha_tuple)		#will print tuple
